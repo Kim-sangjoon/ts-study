0986984e17877cb13f98148ce5ab94c439e35c38
Kim-Sangjoon <sang1009@naver.com>
2023-07-20 17:28:40 +0900

    456

--- test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Calculator = function (a, b) {
-    return a + b;
-};
-console.log(Calculator(1, 2));
-exports.default = Calculator;
